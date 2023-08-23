3e8dcfb340cbef694c6bf9b5f5efa05027d07377
SudoPumpkin <86701852+SudoPumpkin@users.noreply.github.com>
2023-08-22 21:56:22 -0400

    Update multi_tab.spec.ts
    updated example links

--- e2e/Tests/multi_tab.spec.ts
+++ e2e/Tests/multi_tab.spec.ts
@@ -4,8 +4,8 @@ test('Multiple Tabs in 1 Browser', async ({ browser }) => {
     const context = await browser.newContext();
     const page1 = await context.newPage();
     const page2 = await context.newPage();
-     await page1.goto('https://web3.career/');
-     await page2.goto('https://docs.google.com/forms/d/e/1FAIpQLSe1WyQBkVUbL9xdp18twztm2TYhAcrFxwgGNSy9-r7Kw__oTQ/viewform?ref=web3.career&src=web3.career&source=web3.career');
+     await page1.goto('https://tab1');
+     await page2.goto('https://tab2');
      await page1.waitForTimeout(5000);
 
   });
